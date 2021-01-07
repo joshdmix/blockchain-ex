bb5097c009895f91559adc9d01379f7ad52a7094
Josh Mix <joshuadmix@gmail.com>
2021-01-07 15:14:54 -0500

    basic

---
+++ lib/block.ex
@@ -0,0 +1,30 @@
+defmodule Block do
+  defstruct [:data, :timestamp, :prev_hash, :hash]
+
+  @doc "Build a new block for given data and previous hash"
+  def new(data, prev_hash) do
+    %Block{
+      data: data,
+      prev_hash: prev_hash,
+      timestamp: NaiveDateTime.utc_now()
+    }
+  end
+
+  @doc "Build the initial block of the chain"
+  def genesis do
+    %Block{
+      data: "",
+      prev_hash: "",
+      timestamp: NaiveDateTime.utc_now()
+    }
+  end
+
+  @doc "Check if a block is valid"
+  def valid?(%Block{} = block) do
+    Crypto.hash(block) == block.hash
+  end
+
+  def valid?(%Block{} = block, %Block{} = prev_block) do
+    block.prev_hash == prev_block.hash && valid?(block)
+  end
+end
---
+++ lib/blockchain.ex
@@ -0,0 +1,34 @@
+defmodule Blockchain do
+  @doc "Create a new blockchain with a genesis block"
+  def new do
+    [Crypto.put_hash(Block.zero())]
+  end
+
+  @doc "Insert given data as a new block in the blockchain"
+  def insert(blockchain, data) when is_list(blockchain) do
+    %Block{hash: prev} = hd(blockchain)
+
+    block = data |> Block.new(prev) |> Crypto.put_hash()
+
+    [block | blockchain]
+  end
+
+  @doc "Validate the complete blockchain"
+  def valid?(blockchain) when is_list(blockchain) do
+    zero =
+      Enum.reduce_while(blockchain, nil, fn prev, current ->
+        cond do
+          current == nil ->
+            {:cont, prev}
+
+          Block.valid?(current, prev) ->
+            {:cont, prev}
+
+          true ->
+            {:halt, false}
+        end
+      end)
+
+    if zero, do: Block.valid?(zero), else: false
+  end
+end
